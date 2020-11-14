ff03ca71af2e667a0594e754fb5535d3ccb8c77c
Ze Sheng <z7sheng@uwaterloo.ca>
2020-11-14 09:06:02 +0800

    A3 ongoing v5.6

--- a3/rgen.cpp
+++ a3/rgen.cpp
@@ -3,18 +3,23 @@
 #include <unistd.h>
 
 int main (int argc, char **argv) {
-    std::cout << "add \"Amphitheatre Prkw\" (2,-1)(2,2) (5,5) (5,6) (3,8)\n"
-              << "mod \"amphitheatre PRKW\" (2,8)(4,6)(5,6)(7,8)\n"
-              << "add \"One Infinite Loop\" (2,4)(3,6)(6,6)(8,4)\n"
-              << "gg\n"
-              << std::endl;
+    // std::cout << "add \"Amphitheatre Prkw\" (2,-1)(2,2) (5,5) (5,6) (3,8)\n"
+    //           << "mod \"amphitheatre PRKW\" (2,8)(4,6)(5,6)(7,8)\n"
+    //           << "add \"One Infinite Loop\" (2,4)(3,6)(6,6)(8,4)\n"
+    //           << "gg\n"
+    //           << std::endl;
 
-    std::cout << "rm \"Amphitheatre Prkw\"\n"
-              << "rm \"One Infinite Loop\"\n"
-              << "add \"Amphitheatre Prkw\" (2,-1)(2,2) (5,5) (5,6) (3,8)\n"
-              << "add \"One Infinite Loop\" (2,4)(3,6)(6,6)(8,4)\n"
-              << "gg\n"
-              << std::endl;
+    // std::cout << "rm \"Amphitheatre Prkw\"\n"
+    //           << "rm \"One Infinite Loop\"\n"
+    //           << "add \"Amphitheatre Prkw\" (2,-1)(2,2) (5,5) (5,6) (3,8)\n"
+    //           << "add \"One Infinite Loop\" (2,4)(3,6)(6,6)(8,4)\n"
+    //           << "gg\n"
+    //           << std::endl;
+
+    std::cout << "add \"Amphitheatre Prkw\" (2,-1)(2,2) (5,5) (5,6) (3,8)\n" << std::endl;
+    std::cout << "mod \"amphitheatre PRKW\" (2,8)(4,6)(5,6)(7,8)\n" << std::endl;
+    std::cout << "add \"One Infinite Loop\" (2,4)(3,6)(6,6)(8,4)\n" << std::endl;
+    std::cout << "gg\n" << std::endl;
 
     sleep(1);
     sleep(15);
