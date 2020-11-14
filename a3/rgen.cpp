2dddb8b4467072faba0fdf1a8ce74c4528a829ef
Ze Sheng <z7sheng@uwaterloo.ca>
2020-11-15 00:39:12 +0800

    A3 ongoing v1.1.3

--- a3/rgen.cpp
+++ a3/rgen.cpp
@@ -197,7 +197,8 @@ int main(int argc, char **argv) {
                     cntry = 0;
                 }
                 else if (cntry >= limit) {
-                    std::cout << "}" << std::endl;
+                    std::cout << "}\n";
+                    std::cout << "gg" << std::endl;
                     std::cerr << "Error: Failed to generate valid specification\n";
                     urandom.close();
                     return 0;
