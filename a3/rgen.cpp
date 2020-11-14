2418083352697635e839684836551ea0b43c3a72
Ze Sheng <z7sheng@uwaterloo.ca>
2020-11-14 09:25:29 +0800

    A3 ongoing v6.2

--- a3/rgen.cpp
+++ a3/rgen.cpp
@@ -16,10 +16,18 @@ int main (int argc, char **argv) {
     //           << "gg\n"
     //           << std::endl;
 
-    std::cout << "add \"Amphitheatre Prkw\" (2,-1)(2,2) (5,5) (5,6) (3,8)\n" << std::endl;
-    std::cout << "mod \"amphitheatre PRKW\" (2,8)(4,6)(5,6)(7,8)\n" << std::endl;
-    std::cout << "add \"One Infinite Loop\" (2,4)(3,6)(6,6)(8,4)\n" << std::endl;
-    std::cout << "gg\n" << std::endl;
+    std::cout << "add \"Amphitheatre Prkw\" (2,-1)(2,2) (5,5) (5,6) (3,8)" << std::endl;
+    std::cout << "mod \"amphitheatre PRKW\" (2,8)(4,6)(5,6)(7,8)" << std::endl;
+    std::cout << "add \"One Infinite Loop\" (2,4)(3,6)(6,6)(8,4)" << std::endl;
+    std::cout << "gg" << std::endl;
+    sleep(3);
+
+    // std::cout << "rm \"Amphitheatre Prkw\"\n"
+    //           << "rm \"One Infinite Loop\"\n"
+    //           << "add \"Amphitheatre Prkw\" (2,-1)(2,2) (5,5) (5,6) (3,8)\n"
+    //           << "add \"One Infinite Loop\" (2,4)(3,6)(6,6)(8,4)\n"
+    //           << "gg\n"
+    //           << std::endl;
 
     sleep(1);
     sleep(15);
