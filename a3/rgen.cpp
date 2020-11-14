0b60b888adfcee976ec7c624e6e5a0e2f51999e5
Ze Sheng <z7sheng@uwaterloo.ca>
2020-11-14 08:52:44 +0800

    A3 ongoing v5.2

--- a3/rgen.cpp
+++ a3/rgen.cpp
@@ -6,14 +6,14 @@ int main (int argc, char **argv) {
     std::cout << "add \"Amphitheatre Prkw\" (2,-1)(2,2) (5,5) (5,6) (3,8)\n"
               << "mod \"amphitheatre PRKW\" (2,8)(4,6)(5,6)(7,8)\n"
               << "add \"One Infinite Loop\" (2,4)(3,6)(6,6)(8,4)\n"
-              << "gg"
+              << "gg\n"
               << std::endl;
 
     std::cout << "rm \"Amphitheatre Prkw\"\n"
               << "rm \"One Infinite Loop\"\n"
               << "add \"Amphitheatre Prkw\" (2,-1)(2,2) (5,5) (5,6) (3,8)\n"
               << "add \"One Infinite Loop\" (2,4)(3,6)(6,6)(8,4)\n"
-              << "gg"
+              << "gg\n"
               << std::endl;
 
     sleep(1);
