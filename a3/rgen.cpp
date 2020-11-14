53926f55deea1bfdb38b07a21316c00a4c6df485
Ze Sheng <z7sheng@uwaterloo.ca>
2020-11-14 09:28:07 +0800

    A3 ongoing v6.3

--- a3/rgen.cpp
+++ a3/rgen.cpp
@@ -22,14 +22,13 @@ int main (int argc, char **argv) {
     std::cout << "gg" << std::endl;
     sleep(3);
 
-    // std::cout << "rm \"Amphitheatre Prkw\"\n"
-    //           << "rm \"One Infinite Loop\"\n"
-    //           << "add \"Amphitheatre Prkw\" (2,-1)(2,2) (5,5) (5,6) (3,8)\n"
-    //           << "add \"One Infinite Loop\" (2,4)(3,6)(6,6)(8,4)\n"
-    //           << "gg\n"
-    //           << std::endl;
+    std::cout << "rm \"Amphitheatre Prkw\"" << std::endl;
+    std::cout << "rm \"One Infinite Loop\"" << std::endl;
+    std::cout << "add \"Amphitheatre Prkw\" (2,-1)(2,2) (5,5) (5,6) (3,8)" << std::endl;
+    std::cout << "add \"One Infinite Loop\" (2,4)(3,6)(6,6)(8,4)" << std::endl;
+    std::cout << "gg" << std::endl;
 
-    sleep(1);
+    // sleep(1);
     sleep(15);
     return 0;
 }
